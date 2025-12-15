25b12703a4932538d483d35816dc0953ee340883
hanael01 <h.ngadi@niryo.com>
2025-12-15 14:17:55 +0100

    Update utils.py
    Signed-off-by: hanael01 <h.ngadi@niryo.com>

--- src/lerobot/robots/utils.py
+++ src/lerobot/robots/utils.py
@@ -60,6 +60,10 @@ def make_robot_from_config(config: RobotConfig) -> Robot:
         from tests.mocks.mock_robot import MockRobot
 
         return MockRobot(config)
+    elif config.type == "ned2"
+         from .ned2 import Ned2
+
+         return Ned2(config)
     else:
         try:
             return cast(Robot, make_device_from_device_class(config))
